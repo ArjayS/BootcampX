1aaf4907445c2f9d7ff927883e94ecf8262541d8
Robert Servado <servadorobertgmail.com>
2022-08-09 21:52:37 -0400

    Modified the teachers.js to the format to help avoid sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,22 +7,44 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-// Exercise Query 3 @ Node Postgres lecture - Name of Teachers That Assisted
+// // Exercise Query 3 @ Node Postgres lecture - Name of Teachers That Assisted
 
-pool
-  .query(
-    `
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
-ORDER BY teacher;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((row) => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    });
+// pool
+//   .query(
+//     `
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+// ORDER BY teacher;
+// `
+//   )
+//   .then((res) => {
+//     res.rows.forEach((row) => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     });
+//   });
+
+// SQL Injection
+
+const cohortName = process.argv[2] || "JUL02";
+
+const values = [`%${cohortName}%`];
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher;
+  `;
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   });
+});
