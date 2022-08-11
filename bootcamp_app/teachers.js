08828f066adb9459ef5a369ff051f4cb1ba01e45
Robert Servado <servadorobertgmail.com>
2022-08-10 23:53:44 -0400

    Modified teachers.js so that the LIKE is replaced by the original setup of = in finding the right cohorts.name

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -31,7 +31,7 @@ const pool = new Pool({
 
 const cohortName = process.argv[2] || "JUL02";
 
-const values = [`%${cohortName}%`];
+const values = [`${cohortName}`];
 
 const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -39,7 +39,7 @@ const queryString = `
   JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE $1
+  WHERE cohorts.name = $1
   ORDER BY teacher;
   `;
 
